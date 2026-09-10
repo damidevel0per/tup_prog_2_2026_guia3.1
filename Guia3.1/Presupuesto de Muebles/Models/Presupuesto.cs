00892ba8e9849feb72e7e0b0a8fa7c89adbe0abb
damidevel0per <damidevtech@gmail.com>
2026-09-10 11:06:40 -0300

    agregando clases de ejercicio presupuesto de muebles

---
+++ Guia3.1/Presupuesto de Muebles/Models/Presupuesto.cs
@@ -0,0 +1,48 @@
+﻿using Muebles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presupuesto_de_Muebles.Models
+{
+    internal class Presupuesto
+    {
+        private List<Producto> presupuestados = new List<Producto>();
+
+        public int CantidadProductos { get { return presupuestados.Count; } }
+        public double CostoTotal { 
+            get 
+            {
+                double total = 0;
+                for (int i = 0; i < CantidadProductos; i++)
+                {
+                    total += presupuestados[i].CalcularPrecio();
+                }
+                return total; 
+            } 
+        }
+
+        public string Cliente { get; set; }
+        public string Direccion { get; set; }
+
+        public Presupuesto(string client, string direc)
+        {
+            Cliente = client;
+            Direccion = direc;
+        }
+
+        public void AgregarProducto(Producto p)
+        {
+            presupuestados.Add(p);
+        }
+
+        public Producto VerProducto(int idx)
+        {
+            return presupuestados[idx];
+        }
+
+
+    }
+}
---
+++ Guia3.1/Presupuesto de Muebles/Models/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muebles.Models
+{
+    abstract class Producto
+    {
+        private string descripcion;
+        protected double precioBase;
+        protected int tipoMaterial;
+
+        protected string Descripcion { get; }
+
+        public Producto(string desc, double pb, int tipoM)
+        {
+            descripcion = desc;
+            precioBase = pb;
+            tipoMaterial = tipoM;
+        }
+
+        public abstract double CalcularPrecio();
+        public abstract string VerDetalle();
+
+    }
+}
